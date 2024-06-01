c97c8ba0d90f37bf5c597c794398d2dceaddf19f
qod3r <primculov@protonmail.ch>
2024-06-01 13:49:16 +0700

    async predict

--- backend/modellib/utils/__init__.py
+++ backend/modellib/utils/__init__.py
@@ -1,1 +1,1 @@
-from utils.convert import Slice, study_to_slices, normalize_slices, study_to_temp_imgs
+from utils.convert import Slice, study_to_slices, normalize_slices, study_to_temp_imgs, fdata_to_temp_imgs
