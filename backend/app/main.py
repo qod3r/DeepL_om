9bcaa34dd7cddf101b20d6e1894c77d2bd9d3dda
asskicker54 <ass_kicker54@vk.com>
2024-06-02 20:49:31 +0700

    cors added

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.users.router import router as router_users
 from app.study.router import router as router_studies
 
@@ -7,3 +9,16 @@ app = FastAPI()
 app.include_router(router_users)
 app.include_router(router_studies)
 
+origins = [
+    "http://localhost:8000", #Вместо 8000 указываешь порт на котором React 5173
+    "http://127.0.0.1:8000",
+    "http://0.0.0.0:8000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=True,
+    allow_credential=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
